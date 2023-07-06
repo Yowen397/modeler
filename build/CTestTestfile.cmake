73ce569d67d62bf904862c5fea6717a86fb77163
hz_fake <hz@fake.com>
2023-07-06 16:41:55 +0800

    执行git rm -r --cached build/之后再次提交

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/modeler
-# Build directory: /root/modeler/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
