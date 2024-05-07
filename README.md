0455fc48d15573ada13cb69a52c84b50542bf142
Yowen397 <137564817@qq.com>
2024-05-07 18:51:25 +0800

    新增运行脚本

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # modeler
-test gittest
-
+建模智能合约并生成状态空间进行验证
 
 
 
 ### 切换Tokens的方法 ###
 直接注释/取消注释StateSpace.h文件中的宏定义 \
 #define USE_TOKENS
+
+### 运行 ###
+solc_files目录下scripts.py运行可批量生成语法树，对应测试集文件在tests目录下，生成的语法树文件以path/SmartContractName/combined.json格式存在 \
+批量运行：将src目录下的run.py复制到bin目录（cmake生成可执行文件目录）下，执行脚本
---
+++ src/run.py
@@ -0,0 +1,29 @@
+'''
+批量运行测试
+'''
+import glob
+import os
+
+# 测试样例目录
+tests_dir = "../solc_files/tests"
+# 语法树输出目录
+output_dir = "../solc_files/output2"
+
+def get_all_files(directory):
+    file_list = glob.glob(directory+"/*.sol", recursive=True)
+    return file_list
+
+if __name__ == "__main__":
+    file_list = get_all_files(tests_dir)
+    # 运行命令
+    for f in file_list:
+        cmd = "./modeler "
+        basename = os.path.basename(f)
+        basename_without_ext, basename_ext = os.path.splitext(basename)
+        
+        cmd += output_dir+"/"+basename_without_ext+"/combined.json "
+        
+        cmd += " 1>> record.txt 2> error.txt"
+        print(cmd)
+        os.system(cmd)
+    
